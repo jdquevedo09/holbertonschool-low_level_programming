3a31f4e57ab0098a8e011dd16f49300d457b563a
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-16 22:42:44 +0000

    Fixing spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@ int main(void)
 char charType;
 int intType;
 float floatType;
-long longintType;
-long long longlongitType;
-printf("Size of a char: %ld bytes\n", sizeof(charType));
-printf("Size of an int: %ld bytes\n", sizeof(intType));
-printf("Size of a long int: %ld bytes\n", sizeof(longintType));
-printf("Size of a long long int: %ld byte\n", sizeof(longlongintType));
-printf("Size of a float: %ld byte\n", sizeof(floatType));
+long longType;
+long long longlongType;
+printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
