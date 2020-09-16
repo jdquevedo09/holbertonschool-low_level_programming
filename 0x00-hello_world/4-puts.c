fc16ea0be2f9fcf517cde65f8496a3b027db9d36
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-16 20:06:57 +0000

    fixing puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 main(void)
 {
-char a[] = "\"Programming is like building a multilingual puzzle";
-puts(a);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
