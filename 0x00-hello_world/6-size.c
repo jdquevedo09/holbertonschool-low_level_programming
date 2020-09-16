ebe9d91df1dbbf63c924660417d862d0320ed356
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-16 23:00:47 +0000

    Using direct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,10 @@
  */
 int main(void)
 {
-char charType;
-int intType;
-float floatType;
-long longType;
-long long longlongType;
-printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-printf("Size of an int: %ld byte(s)\n", sizeof(intType));
-printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
-printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
