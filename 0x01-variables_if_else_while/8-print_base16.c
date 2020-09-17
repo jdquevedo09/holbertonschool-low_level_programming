bb88de3b78aa16f0fd3b148196a4e1e5fabcb2d3
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-17 23:52:15 +0000

    Hey Look ma i made it

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Void
+ *Return: 0
+ */
+int main(void)
+{
+int x = 48;
+char y = 'a';
+for (x = 48 ; x <= 57 ; x++)
+{
+putchar(x);
+}
+while (y <= 'f')
+{
+putchar(y);
+y++;
+}
+putchar('\n');
+return (0);
+}
