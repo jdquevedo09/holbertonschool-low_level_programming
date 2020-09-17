477263c16d3c63c1ec8a40eb7626503d8fd556de
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-17 22:42:38 +0000

    Plox

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@ int main(void)
 char x = 'a';
 while (x < 'z')
 {
-if (x == 'e' || x == 'q')
+if (x != 'e' && x != 'q')
 {
+putchar(x);
 x++;
 }
- else {
-   putchar(x);
-   x++;
- }
+else{
+x++;
+}
 }
 putchar('\n');
 return (0);
