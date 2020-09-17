ba6d2c63e06b9f51e33de0169f6aa1deba6809af
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-17 20:52:04 +0000

    Changing position

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,13 +15,12 @@ if (last > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
+if (last < 6)                                                                                                                 {
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+}
 if (last == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last);
-}
-if (last < 6)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 return (0);
 }
