287b48ade9ecc0e621bb4b9cacad41ef816de65f
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-17 00:00:50 +0000

    Adding library

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - Void
  *Return: Always 0
@@ -6,5 +7,5 @@
 int main(void)
 {
 write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (0);
+return (1);
 }
