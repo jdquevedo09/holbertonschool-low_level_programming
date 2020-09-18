68b04fbb40709313eff8247b286cb0e39c5e1077
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-18 00:05:22 +0000

    Idk

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Void
+ *Return: 0
+ */
+int main(void)
+{
+int x = 48;
+for (x = 48 ; x <= 57 ; x++)
+{
+putchar(x);
+putchar(44);
+putchar(32);
+}
+putchar('\n');
+return (0);
+}
