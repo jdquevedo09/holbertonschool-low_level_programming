2ad5dc213a59de7442880564f402eec4c38af48c
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-18 02:06:33 +0000

    Dios por favor

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - Void
+*Return: 0
+*/
+int main(void)
+{
+int x = 48;
+int y = 48;
+for (x = 48 ; x < 58 ; x++)
+{
+for (y = 48; y < 58 ; y++)
+{
+if(x<y){
+putchar(x);
+putchar(y);
+if (x == 56 && y == 57)
+{
+break;
+}
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
