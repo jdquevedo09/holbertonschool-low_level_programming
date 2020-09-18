1d77efce97c6c2f1ad347c687b2e483508b51756
jdquevedo09 <jdquevedo09@gmail.com>
2020-09-18 01:36:36 +0000

    Fe

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - Void
+ *Return: 0
+ */
+int main(void)
+{
+int x = 48;
+int y = 48;
+for (x = 48 ; x < 58 ; x++) 
+{
+for (y = 48; y < 58 ; y++)
+{
+putchar(x);
+putchar(y);
+if (x != 57)
+{
+if (y != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
